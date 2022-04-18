86b6e0db992e6fbe82dac09804245e1d57ae1c97
minsoo <minsu2344@naver.com>
2022-04-18 21:20:35 +0900

    두 숫자 비교

---
+++ 1.2.js
@@ -0,0 +1,26 @@
+// 비교하기
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', line => {
+  input = line.split(' ');
+  rl.close();
+}).on('close', () => {
+  let a = parseInt(input[0]);
+  let b = parseInt(input[1]);
+  if(a > b) {
+    console.log('>');
+  }
+  else if(a === b) {
+    console.log('==');
+  }
+  else {
+    console.log('<');
+  }
+  process.exit();
+});
